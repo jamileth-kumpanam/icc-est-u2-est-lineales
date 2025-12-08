3cc3adba2d5649921d23c6bf2568aa760ad35974
jamileth-kumpanam <jamilethkumpanam@MacBook-Air.local>
2025-12-08 12:50:26 -0500

    Listas enlazadas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import models.Node;
+import models.Person;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Node <Person> node1 = 
+        new Node <Person>(new Person("Juan", 30));
+
+        Node <Person> node2= 
+        new Node <Person>(new Person("Maria", 35));
+
+        Node <Person> node3 = 
+        new Node <Person>(new Person("Pepito", 0));
+
+        Node <Person> node4 = 
+        new Node <Person>(new Person("Diego", 0));
+
+        node1.setNext(node2); //J -> M
+        node2.setNext(node3); //M -> P
+        node3.setNext(node4); // P -> D
+
+        System.out.println(node1);
     }
 }
